30cd3d61a82b2c7536d056aecd5895958d045ddb
Dawid Majdański <dawid.majdanski@britenet.com>
2019-02-22 12:26:33 +0100

    Orders tab

---
+++ src/aura/BM_OrdersTab/BM_OrdersTabController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Majdan on 22.02.2019.
+ */
+({
+    handleMyOrders: function(component, event, helper){
+        var navEvt = $A.get('e.force:navigateToURL');
+        if(navEvt){
+            navEvt.setParams({url: '/my-orders'});
+            navEvt.fire();
+        }
+    },
+})
