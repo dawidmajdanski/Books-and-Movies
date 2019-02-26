c95b1d70cd63c33c7abf8f296e03184f1af0a65c
Dawid Majdański <dawid.majdanski@britenet.com>
2019-02-26 10:38:05 +0100

    Stars rating component fixes

--- src/aura/BM_StarsRating/BM_StarsRatingController.js
+++ src/aura/BM_StarsRating/BM_StarsRatingController.js
@@ -1,30 +1,30 @@
 ({
     afterScriptsLoaded : function(component, event, helper) {
-        var domEl = component.find("ratingarea").getElement();
-        var currentRating = component.get('v.value');
-        var readOnly = component.get('v.readonly');
-        var maxRating = 5;
-        var callback = function(rating) {
+        let domEl = component.find("ratingarea").getElement();
+        let currentRating = component.get('v.value');
+        let readOnly = component.get('v.readonly');
+        let maxRating = 5;
+        let callback = function(rating) {
             component.set('v.value',rating);
         }
         component.ratingObj = rating(domEl,currentRating,maxRating,callback,readOnly);
     },
     onValueChange: function(component,event,helper) {
         if (component.ratingObj) {
-             var value = component.get('v.value');
+             let value = component.get('v.value');
              component.ratingObj.setRating(value,false);
         }
     },
     handleVoteBarToggleOver: function(component, event, helper){
-        var element = document.getElementById("changeStarMouseOver");
+        let element = document.getElementById("changeStarMouseOver");
         element.style.display="block";
-        var left  = 35 + "px";
-        var top  = -20 + "px";
+        let left  = 35 + "px";
+        let top  = -20 + "px";
         element.style.left = left;
         element.style.top = top;
     },
     handleVoteBarToggleLeave: function(component, event, helper){
-        var element = document.getElementById("changeStarMouseOver");
+        let element = document.getElementById("changeStarMouseOver");
         element.style.display="none";
     },
 })
